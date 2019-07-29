a649605f844137b45802dfb11331fca775fee5d6
qinghua.shao <qinghua.shao@weimob.com>
2019-07-29 14:48:02 +0800

    add cat

--- src/main/java/com/example/demo/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CategoryServiceImpl.java
@@ -1,9 +1,13 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.service.CategoryService;
+
 /**
  * @author qinghua.shao
  * @date 2019/7/29
  * @since 1.0.0
  */
-public class CategoryServiceImpl {
+public class CategoryServiceImpl implements CategoryService {
+
+
 }
