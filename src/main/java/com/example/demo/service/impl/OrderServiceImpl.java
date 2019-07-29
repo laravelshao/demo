4bcc52547f059b16e74114c4fe27ecdd4efe37ae
qinghua.shao <qinghua.shao@weimob.com>
2019-07-29 14:40:10 +0800

    commit

--- src/main/java/com/example/demo/service/impl/OrderServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/OrderServiceImpl.java
@@ -1,9 +1,13 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.service.OrderService;
+
 /**
  * @author qinghua.shao
  * @date 2019/7/29
  * @since 1.0.0
  */
-public class OrderServiceImpl {
+public class OrderServiceImpl implements OrderService {
+
+
 }
